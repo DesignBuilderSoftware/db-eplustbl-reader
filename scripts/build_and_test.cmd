ca4dc99e1d6f5745d988c5ceaf65f17a0d5d67f6
voightp <vojtechpanek@seznam.cz>
2021-11-02 09:34:32 +0100

    pass args as list

--- scripts/build_and_test.cmd
+++ scripts/build_and_test.cmd
@@ -1,4 +1,4 @@
-call build.bat db-temperature-distribution
+call build.bat "db-temperature-distribution 0.1.0"
 for %%a in ("%cd%") do set "root=%%~dpa"
 set pytestexe=%root%.venv\Scripts\pytest.exe
-%pytestexe% %root%tests/test_package.py
+%pytestexe% %root%tests/test_executable.py
--- tests/test_executable.py
+++ tests/test_executable.py
@@ -10,8 +10,8 @@ def test_exe_exists(exe_path):
 
 @pytest.mark.parametrize("output", ["csv"])
 def test_exe(exe_path, html_path, test_tempdir, output):
-    command = f"{exe_path} -i {html_path} -o {test_tempdir} --no-dialogs"
-    subprocess.run(command, cwd=exe_path.parent, shell=True)
+    args = [exe_path, "-i", html_path, "-o", test_tempdir, "--no-dialogs"]
+    subprocess.run(args)
     for name in [
         "Distribution - ZONE MEAN AIR TEMPERATURE.csv",
         "Distribution - ZONE OPERATIVE TEMPERATURE.csv",
